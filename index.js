3b24c6ed2f6bcb78500004414549f2e86e93dc8d
sheyllameazza <101612694+sheyllameazza@users.noreply.github.com>
2022-04-25 22:14:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$("#btnlogin").click(
+    function(){
+
+        console.log("O valor do campo email é:");
+        console.log(
+            document.getElementById('imputEmail').value
+        );
+    }
+)
